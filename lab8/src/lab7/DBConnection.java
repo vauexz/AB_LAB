0349ef9d16fc5207f902e7103145af7f30c0f207
vauexz <vauexz@gmail.com>
2020-05-01 10:12:44 +0200

    clean

--- lab8/src/lab7/DBConnection.java
+++
@@ -1,8 +0,0 @@
-package lab7;
-
-
-public class DBConnection {
-	public static String url = "jdbc:sqlserver://192.168.1.15:1433;databaseName=lab6;";
-	public static String user = "sa";
-	public static String password = "1234";
-}
